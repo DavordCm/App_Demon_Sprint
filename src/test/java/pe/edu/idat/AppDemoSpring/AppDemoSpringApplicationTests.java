861314b19b393a43cb22fcc523aa84d62931f6bc
davordcm <Jhoosman.xd@gmail.com>
2024-05-30 20:10:09 -0500

    New Contribution

---
+++ src/test/java/pe/edu/idat/AppDemoSpring/AppDemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.AppDemoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDemoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
